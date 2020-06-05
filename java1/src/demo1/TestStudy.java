0d1610f01e8867cef1474932cbad2d3ea077b1a7
lyh-git <1374061712@qq.com>
2020-06-05 19:47:30 +0800

    测试

---
+++ java1/src/demo1/TestStudy.java
@@ -0,0 +1,15 @@
+package demo1;
+
+import org.junit.Test;
+
+public class TestStudy {
+    @Test
+    public void test1(){
+        char test1='a'+'b';
+        int test2='a'+'b';
+        char test3='中';
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+    }
+}
